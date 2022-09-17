afd2d88476fe3215e98fde189defc7f39f885cba
Nishant <nishantswan03@gmail.com>
2022-09-17 22:31:58 +0530

    second commit

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://nishant-secrets:test123@clustersecrets.bprdsjp.mongodb.net/test", {useNewUrlParser: true});
 mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
